adacad3ece5e85c4b5187c74c743e7d839981889
Michał Galas <michal@webspirit.ie>
2016-02-08 12:28:16 +0100

    galeria

--- js/main.js
+++ js/main.js
@@ -51,60 +51,7 @@ function main() {
 		})
 
 		$(document).ready(function() {
-			$("#team").owlCarousel({
-
-				navigation: false, // Show next and prev buttons
-				slideSpeed: 300,
-				paginationSpeed: 400,
-				autoHeight: true,
-				itemsCustom: [
-					[0, 1],
-					[450, 2],
-					[600, 2],
-					[700, 2],
-					[1000, 4],
-					[1200, 4],
-					[1400, 4],
-					[1600, 4]
-				],
-			});
-
-			$("#clients").owlCarousel({
-
-				navigation: false, // Show next and prev buttons
-				slideSpeed: 300,
-				paginationSpeed: 400,
-				autoHeight: true,
-				itemsCustom: [
-					[0, 1],
-					[450, 2],
-					[600, 2],
-					[700, 2],
-					[1000, 4],
-					[1200, 5],
-					[1400, 5],
-					[1600, 5]
-				],
-			});
-
-			$("#testimonial").owlCarousel({
-				navigation: false, // Show next and prev buttons
-				slideSpeed: 300,
-				paginationSpeed: 400,
-				singleItem: true
-			});
-
-
-			$(".fancybox").fancybox({
-				helpers: {
-					title: {
-						type: 'outside'
-					},
-					overlay: {
-						speedOut: 0
-					}
-				}
-			});
+			$("a[rel^='prettyPhoto']").prettyPhoto({social_tools:''});
 
 		});
 
